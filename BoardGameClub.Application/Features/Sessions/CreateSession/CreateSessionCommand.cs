9d13c8964c9c8e70f42facb1eacdc593f9724d9f
mpbstrd <matthewperrybustarde@gmail.com>
2026-05-07 20:53:02 +0800

    added a sample create session application feature

---
+++ BoardGameClub.Application/Features/Sessions/CreateSession/CreateSessionCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+
+namespace BoardGameClub.Application.Features.Sessions.CreateSession
+{
+    public class CreateSessionCommand : IRequest<Guid>
+    {
+        public Guid HostMemberId { get; set; }
+        public Guid BoardGameId { get; set; }
+        public Guid LocationId { get; set; }
+        public DateTime ScheduledDate { get; set; }
+        public int MaxPlayers { get; set; }
+        public string? Notes { get; set; }
+    }
+}
