25a25b41a99af1f341a3444232d2465143eca522
mpbstrd <matthewperrybustarde@gmail.com>
2026-05-11 21:52:53 +0800

    added empty category app features

---
+++ BoardGameClub.Application/Features/Categories/GetCategories/GetCategoriesHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoardGameClub.Application.Features.Categories.GetCategories
+{
+    internal class GetCategoriesHandler
+    {
+    }
+}
---
+++ BoardGameClub.Application/Features/Categories/GetCategories/GetCategoriesQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoardGameClub.Application.Features.Categories.GetCategories
+{
+    public class GetCategoriesQuery
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+    }
+}
